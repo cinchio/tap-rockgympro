4891a5b83c2c6e0c1bf36e7c91a4a75e323cf57f
Chris London <me@chrislondon.co>
2021-08-27 08:41:49 -0600

    Using singer prefix for imports

--- tap_rockgympro/__init__.py
+++ tap_rockgympro/__init__.py
@@ -1,17 +1,17 @@
 import json
 
-from singer import utils, get_logger
+import singer
 from tap_rockgympro.syncer import Syncer
 from tap_rockgympro.utils import discover
 
-LOGGER = get_logger()
+LOGGER = singer.get_logger()
 REQUIRED_CONFIG_KEYS = ['api_user', 'api_key']
 
 
-@utils.handle_top_exception(LOGGER)
+@singer.utils.handle_top_exception(LOGGER)
 def main():
     # Parse command line arguments
-    args = utils.parse_args(REQUIRED_CONFIG_KEYS)
+    args = singer.utils.parse_args(REQUIRED_CONFIG_KEYS)
 
     # If discover flag was passed, run discovery mode and dump output to stdout
     if args.discover:
