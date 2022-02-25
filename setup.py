ef3f0733da59d59a40171828e600c1c77565d288
Chris London <me@chrislondon.co>
2022-02-24 17:26:47 -0700

    adding in requests requirement

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="tap-rockgympro",
-    version="0.1.13",
+    version="0.1.14",
     description="Singer.io tap for extracting RockGymPro data",
     author="Cinch",
     url="https://github.com/cinchio/tap-rockgympro",
     python_requires='>=3.6.0',
     py_modules=["tap_rockgympro"],
     install_requires=[
-        "singer-python==5.12.1"
+        "singer-python==5.12.1",
+        "requests==2.26.0"
     ],
     entry_points="""
     [console_scripts]
