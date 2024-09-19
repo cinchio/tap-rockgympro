e494ee1f5b01d8ff3ec4b3e67c2dd65267df5d38
Austin Bradford <austin.bradford@cinch.io>
2024-09-19 12:03:09 -0600

    forgot to version

--- setup.py
+++ setup.py
@@ -4,23 +4,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="tap-rockgympro",
-    version="0.1.15",
+    version="0.1.16",
     description="Singer.io tap for extracting RockGymPro data",
     author="Cinch",
     url="https://github.com/cinchio/tap-rockgympro",
-    python_requires='>=3.6.0',
+    python_requires=">=3.6.0",
     py_modules=["tap_rockgympro"],
-    install_requires=[
-        "singer-python==5.12.1",
-        "requests==2.26.0"
-    ],
+    install_requires=["singer-python==5.12.1", "requests==2.26.0"],
     entry_points="""
     [console_scripts]
     tap-rockgympro=tap_rockgympro:main
     """,
-    packages=find_packages(include=['tap_rockgympro', 'tap_rockgympro.*']),
-    package_data = {
-        "tap_rockgympro": ["schemas/*.json"]
-    },
+    packages=find_packages(include=["tap_rockgympro", "tap_rockgympro.*"]),
+    package_data={"tap_rockgympro": ["schemas/*.json"]},
     include_package_data=True,
 )
